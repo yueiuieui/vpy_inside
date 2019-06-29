79982722ffe3591f29c88c2b92a140e864175732
yueiuieui <35925193+yueiuieui@users.noreply.github.com>
2019-06-29 16:36:11 +0800

    build_lib

---
+++ lib.py
@@ -0,0 +1,17 @@
+﻿def vin(word):
+    return input(word)
+def vout(word):
+    return print(word)
+def write_file(file,mode,content):
+    with open(file,mode) as f:
+        f.write(content)
+    return 0
+def read_file(file,mode):
+    with open(file,mode) as f:
+        return f.read()
+def until(exp,fuc):
+    while exp == True:
+        return fuc
+        break
+    else:    
+        continue
