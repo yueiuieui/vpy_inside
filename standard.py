f8a29f2e5bb7f4f10947b47db096fc487f6c821c
yueiuieui <35925193+yueiuieui@users.noreply.github.com>
2019-07-09 19:11:55 +0800

    Update standard.py

--- standard.py
+++ standard.py
@@ -11,10 +11,27 @@ def run(pro):
     elif sv == 'Linux':
         os.system('sudo python3 '+pro)
 global ver
+def getResult(whatgetresult):
+    return whatgetresult
+    
 ver="inside"
 def vin(word):
+    '''This is a input fuction,it can get what user entered.
+    For example:
+    >>a=vin("hi_")
+    hi_t
+    >>vout(a)
+    "t"'''
     return input(str(word))
 def vout(word):
+    '''This is a output fuction,it can show what did you say to user.
+    For example:
+    >>vout(i)
+    i
+    >>vout(3=5)
+    False
+    >>vout(3+5)
+    8'''
     print(word)
 def write_file(file,mode,content):
     with open(file,mode) as f:
